4c23839c93ca8a95227264d614c8019335ba926f
CryptoRaptor <dan.eve@hotmail.com>
2020-02-28 23:54:12 -0800

    Twks

--- Sandbox.sh
+++ Sandbox.sh
@@ -1,8 +1,11 @@
+# jq '.assets,.ref' $HOME/j.json
 
-$nodestart
-$nodestop
-$dgld getbalance
-$cbt getbalance
-$gnstart
-$logs
-$cc
+
+# jq '.n,.m,.height' $HOME/j.json
+
+
+# cat $HOME/j.json | grep ref
+
+jq '.blockNumber' $HOME/peg.json
+
+# cat $HOME/j.json| jq '.assets.headers | [.sender, .to, .subject, ."x-received-time", .received, .from, .date, .id, .to, .subject, .fromfull] + [(.time | tostring)] | join(", ")'
