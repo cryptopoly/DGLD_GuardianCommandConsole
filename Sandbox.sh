373cf29d83c70ca4e47ef70c09b8216c8d13e8c6
CryptoRaptor <dan.eve@hotmail.com>
2020-03-10 22:01:21 +0000

    Fixes

--- Sandbox.sh
+++ Sandbox.sh
@@ -12,7 +12,4 @@
 # cat $HOME/j.json| jq '.assets.headers | [.sender, .to, .subject, ."x-received-time", .received, .from, .date, .id, .to, .subject, .fromfull] + [(.time | tostring)] | join(", ")'
 
 
-#!/bin/bash
-
-jq | $HOME/ocean/ocean-cli -datadir=$HOME/dgld/mainnet/ocean-cb help | awk '{ print ++lvalue, $0 }'
 
