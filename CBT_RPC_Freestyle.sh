a400fa2482932bcb35cfd5cb60d5b1946177aa25
CryptoRaptor <dan.eve@hotmail.com>
2020-03-11 16:39:17 +0000

    Fixes/menu additions

--- CBT_RPC_Freestyle.sh
+++ CBT_RPC_Freestyle.sh
@@ -6,8 +6,7 @@ while true; do
 	echo "Enter CBT ocean-cli RPC command e.g. getblockchaininfo, ('00' to exit)"
 	read freestyle
 	if [[ -z $freestyle ]] ; then exec
-	elif [[ $freestyle == "00" ]] ; then exit ; echo "Exiting DGLD-CBT Guardian Command Launcher"; echo ""; exit
-	elif ! (($freestyle >= 0 && $freestyle <= $freestyle)) ; then exec
+	elif [[ $freestyle == "00" ]] ; then exit ;
 else
 	docker exec guardnode_ocean-cb_1 ocean-cli -rpcport=8332 -rpcuser=ocean -rpcpassword=oceanpass $freestyle
 	echo ""
